d3cc3cbbbea2c5086c191de93b51ae49f9fe97a5
uzzol101 <davepillowgood@gmail.com>
2016-10-26 17:46:34 +0600

    HeroDetailComponent created, waiting for connection to app

---
+++ app/hero-detail.component.ts
@@ -0,0 +1,13 @@
+import {Component,Input} from '@angular/core';
+import {Hero} from './hero';
+
+@Component({
+	moduleId:module.id,
+	selector:'my-hero-detail',
+	templateUrl:'hero-detail.component.html'
+})
+
+export class HeroDetailComponent{
+
+	
+}
